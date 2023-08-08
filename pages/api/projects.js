3fed3adbf1379c2a6645f48015b00c50af29d3cb
Sobarzo14 <63866666+Sobarzo14@users.noreply.github.com>
2023-08-08 02:58:40 -0400

    Deployed to vercel

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -1,15 +1,18 @@
 export const projects = [
     {
+        key: 0,
         image: '/../public/projects/nature.png',
         alt: 'Nature',
         title: 'Nature',
     },
     {
+        key: 1,
         image: '/../public/projects/weather.png',
         alt: 'Weather',
         title: 'Weather',
     },
     {
+        key: 2,
         image: '/../public/projects/voyage.png',
         alt: 'Voyage',
         title: 'Voyage',
